d8356490c0cce70944bec86fa5bf473d48d9de9b
Maxence Laurent <maxence.laurent@gmail.com>
2020-12-14 14:02:26 +0100

    another test

--- src/main/java/com/github/maxencelaurent/javapaths/Main.java
+++ src/main/java/com/github/maxencelaurent/javapaths/Main.java
@@ -6,8 +6,12 @@
 
 package com.github.maxencelaurent.javapaths;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -17,20 +21,35 @@ import java.nio.file.Paths;
  */
 public class Main {
 
-    private static final String srcPath = "com/github/maxencelaurent/javapaths/Main.class";
+    private static final String srcPath = "src/main/resources/godot.txt";
 
-    public void testPath() {
+    public void testPath() throws IOException {
         String currentPath = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+
+        int indexOf = currentPath.indexOf("/target");
+        String root = currentPath.substring(0, indexOf);
+
         System.out.println("CurrentPath: " + currentPath);
-        Path get = Paths.get(currentPath, srcPath);
+        Path get = Paths.get(root, srcPath);
         System.out.println("Get " + get);
+        String readString = Files.readString(get);
+        System.out.println(readString);
     }
 
-    public void testPathUri() throws URISyntaxException {
+    public void testPathUri() throws URISyntaxException, MalformedURLException, IOException {
         URL currentPath = getClass().getProtectionDomain().getCodeSource().getLocation();
+        String path = currentPath.toString();
+        int indexOf = path.indexOf("target");
+
+        String root = path.substring(0, indexOf);
+        URL url = new URL(root +srcPath);
         System.out.println("CurrentPathURL: " + currentPath);
-        Path get = Paths.get(currentPath.toURI());
+        Path get = Paths.get(url.toURI());
+
         System.out.println("toUri.toPath " + get);
+
+        String readString = Files.readString(get);
+        System.out.println(readString);
     }
 
     public static void main(String... argv) {
